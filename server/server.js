04ac9b9a144c8750910ff17db824cfec913164f2
Dorian Wojda <info@it-wojda.de>
2018-07-27 09:51:31 +0200

    Add POST /todos

--- server/server.js
+++ server/server.js
@@ -7,6 +7,24 @@ var {User} = require('./models/User');
 
 var app = express();
 
+//Check midware again
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  //console.log(req.body);
+  var text = req.body.text;
+  var newTodo = new Todo({text});
+  newTodo.save().then((doc) => {
+    res.status(200).send(doc);
+  }, (err) => {
+    if (err) {
+    res.status(400).send(err);
+    }
+  });
+});
+
+
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
