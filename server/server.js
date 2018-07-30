a24876560e3bbdf3052788f025705380e06705ea
Dorian Wojda <info@it-wojda.de>
2018-07-30 18:52:04 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -38,10 +39,9 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var {id} = req.params;
-//check if is ID valid if not send 400 send empty body
-if (!ObjectID.isValid(id)) {
-  return res.status(400).send({error:'Invalid ID'});
-}
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send({error:'Invalid ID'});
+  }
 
   Todo.findById(id).then((todo) => {
     if (todo) {
@@ -58,10 +58,9 @@ if (!ObjectID.isValid(id)) {
 
 app.delete('/todos/:id', (req, res) => {
   var {id} = req.params;
-//check if is ID valid if not send 400 send empty body
-if (!ObjectID.isValid(id)) {
-  return res.status(400).send({error:'Invalid ID'});
-}
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send({error:'Invalid ID'});
+  }
 
   Todo.findByIdAndRemove(id).then((todo) => {
     if (todo) {
@@ -76,7 +75,35 @@ if (!ObjectID.isValid(id)) {
   })
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var {id} = req.params;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send({error:'Invalid ID'});
+  }
 
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
